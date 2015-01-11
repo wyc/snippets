c0527af3b1c9e0efda7bd9f55df1bf93f0321419
wayne <wayne@fea.st>
2015-01-10 20:17:19 -0500

    updates

--- haskell/cond.hs
+++ haskell/cond.hs
@@ -3,3 +3,5 @@ signum x = if x < 0 then -1 else 1
 
 formalSignum 0 = 0
 formalSignum x = (/) (abs x) x
+
+
---
+++ haskell/pm.hs
@@ -0,0 +1,5 @@
+first (x, _, _) = x  
+  
+second (_, y, _) = y  
+    
+third (_, _, z) = z  
